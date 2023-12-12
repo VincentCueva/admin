4e9114ce1119b7c97c873378e448825291ac2d63
VincentCueva <vincentcueva08@gmail.com>
2023-12-13 05:49:13 +0800

    init

--- script.js
+++ script.js
@@ -2,47 +2,33 @@ $(document).ready(function() {
     
     $('#userTable').DataTable();
 
-    
     $('.edit-btn').on('click', function() {
         var userId = $(this).data('id');
         window.location.href = 'edit_user.php?id=' + userId;
     });
 
-    
     $('.delete-btn').on('click', function() {
-        var userId = $(this).data('id');
+        var userID = $(this).data('id');
 
-        
-        Swal.fire({
-            title: 'Are you sure?',
-            text: 'This action cannot be undone!',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#d33',
-            cancelButtonColor: '#3085d6',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                
-                $.ajax({
-                    type: 'POST',
-                    url: 'delete_user.php',
-                    data: { id: userId },
-                    dataType: 'json',
-                    success: function(response) {
-                        if (response.success) {
-                            
-                            location.reload();
-                        } else {
-                            Swal.fire('Error', response.message, 'error');
-                        }
-                    },
-                    error: function(error) {
-                        console.error('Error deleting user:', error);
-                        Swal.fire('Error', 'An unexpected error occurred.', 'error');
-                    }
-                });
-            }
-        });
+        if (confirm('Are you sure you want to delete this user?')) {
+            $.ajax({
+                type: 'POST',
+                url: 'delete_user.php',
+                data: { id: userID },
+                dataType: 'json'
+            })
+            .done(function(response) {
+                if (response.success) {
+                    location.reload();
+                } else {
+                    console.error('Error deleting user:', response.message);
+                    alert('Error deleting user. Please check the console for details.');
+                }
+            })
+            .fail(function(xhr, status, error) {
+                console.error('AJAX error:', xhr.responseText);
+                alert('AJAX error. Please check the console for details.');
+            });
+        }
     });
 });
